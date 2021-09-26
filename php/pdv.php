101a69bee228a184b5b1e20a78f737a18fe651bd
DicoLOF <osvaldo_filho@live.com>
2021-09-26 12:38:17 -0300

    Primeiro Commit

---
+++ php/pdv.php
@@ -0,0 +1,257 @@
+<div class="wrapper">
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="container-fluid">
+                <div class="row mb-2">
+                    <div class="col-sm-6">
+                        <h1>Ponto de Venda</h1>
+                    </div>
+                    <div class="col-sm-6">
+                        <ol class="breadcrumb float-sm-right">
+                            <li class="breadcrumb-item"><a href="main">Home</a></li>
+                            <li class="breadcrumb-item active">PDV</li>
+                        </ol>
+                    </div>
+                </div>
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- Main content -->
+        <section class="content">
+            <div class="container-fluid">
+                <!-- PONTO DE VENDA -->
+                <div class="card card-default">
+                    <div class="card-header">
+                        <h3 class="card-title">Venda</h3>
+                        <div class="card-tools">
+                            <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i></button>
+                            <button type="button" class="btn btn-tool" data-card-widget="remove"><i class="fas fa-times"></i></button>
+                        </div>
+                    </div>
+                    <!-- /.card-header -->
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <label>Cliente</label>
+                                    <select class="form-control select2" style="width: 100%;">
+                                        <option selected="selected">Alabama</option>
+                                        <option>Alaska</option>
+                                        <option>California</option>
+                                        <option>Delaware</option>
+                                        <option>Tennessee</option>
+                                        <option>Texas</option>
+                                        <option>Washington</option>
+                                    </select>
+                                </div>
+
+                            </div>
+                            <!-- /.col -->
+                            <div class="col-md-6">
+                                <!-- /.form-group -->
+                                <div class="form-group">
+                                    <label>Situação</label>
+                                    <select class="form-control select2" style="width: 100%;">
+                                        <option selected="selected">Normal</option>
+                                        <option>Inadimplente</option>                                        
+                                    </select>
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+
+                        <h5>Codigo Produto</h5>
+                        <div class="row">
+                            <div class="col-12 col-sm-6">
+                                <div class="form-group">
+                                    <label>Codigo de Barras ou Reduzido</label>                                
+                                    <input class="form-control form-control-lg" type="text" placeholder="Codigo Produto">                                
+                                </div>
+                                <!-- /.form-group -->
+                            </div>
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.card-body -->
+
+                </div>
+                <!-- /.card -->
+                <!-- /.row -->
+            </div><!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+
+        <!-- PDV TABELA DE COMPRAS -->
+        <section class="content">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <h3 class="card-title">LISTA DE COMPRAS</h3>
+                            </div>
+                            <!-- /.card-header -->
+                            <div class="card-body">
+                                <table id="example1" class="table table-bordered table-hover">
+                                    <thead>
+                                        <tr>
+                                            <th>Descrição</th>
+                                            <th>Preço</th>
+                                            <th>Quantidade</th>
+                                            <th>Subtotal</th>
+                                            <th>Excluir</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet
+                                                Explorer 4.0
+                                            </td>
+                                            <td>Win 95+</td>
+                                            <td> 4</td>
+                                            <td>X</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet
+                                                Explorer 5.0
+                                            </td>
+                                            <td>Win 95+</td>
+                                            <td>5</td>
+                                            <td>C</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet
+                                                Explorer 5.5
+                                            </td>
+                                            <td>Win 95+</td>
+                                            <td>5.5</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet
+                                                Explorer 6
+                                            </td>
+                                            <td>Win 98+</td>
+                                            <td>6</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>Internet Explorer 7</td>
+                                            <td>Win XP SP2+</td>
+                                            <td>7</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Trident</td>
+                                            <td>AOL browser (AOL desktop)</td>
+                                            <td>Win XP</td>
+                                            <td>6</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Firefox 1.0</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td>1.7</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Firefox 1.5</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td>1.8</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Firefox 2.0</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td>1.8</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Firefox 3.0</td>
+                                            <td>Win 2k+ / OSX.3+</td>
+                                            <td>1.9</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Camino 1.0</td>
+                                            <td>OSX.2+</td>
+                                            <td>1.8</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Camino 1.5</td>
+                                            <td>OSX.3+</td>
+                                            <td>1.8</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Netscape 7.2</td>
+                                            <td>Win 95+ / Mac OS 8.6-9.2</td>
+                                            <td>1.7</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Netscape Browser 8</td>
+                                            <td>Win 98SE+</td>
+                                            <td>1.7</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Gecko</td>
+                                            <td>Netscape Navigator 9</td>
+                                            <td>Win 98+ / OSX.2+</td>
+                                            <td>1.8</td>
+                                            <td>A</td>
+                                        </tr>
+                                        <tr>
+                                            <td>Blusinha de poliester G</td>
+                                            <td>R$ 59,90</td>
+                                            <td>2 UND</td>
+                                            <td>R$ 119,80</td>
+                                            <td><a href=""><i class="fas fa-times-circle"></i></a></td>
+                                        </tr>
+                      
+                                         
+                                    </tbody>
+                                    <tfoot>
+                                        <tr>
+                                            <th>Descrição</th>
+                                            <th>Preço</th>
+                                            <th>Quantidade</th>
+                                            <th>Subtotal</th>
+                                            <th>Excluir</th>
+                                        </tr>
+                                    </tfoot>
+                                </table>
+                            </div>
+                            <!-- /.card-body -->
+                        </div> 
+                    </div>
+                    <!-- /.col -->
+                </div>
+                <!-- /.row -->
+            </div>
+            <!-- /.container-fluid -->
+        </section>
+        <!-- /.content -->
+    </div>
+</div>
+
+
+
---
+++ php/url.php
@@ -0,0 +1,32 @@
+<?php
+set_include_path("php");
+
+if (isset($_GET['pg'])) {
+    if (substr_count($_GET['pg'], "/") > 0) {
+        $pagina = explode("/", $_GET['pg']);
+        if (!empty($pagina[1])) {
+            if (is_file('./' . $pagina[1] . '.php')) {
+                include $pagina[1] . '.php';
+            } else {
+                echo "<script>alert ('Essa não e uma pagina valida!') ; </script>";
+                header("Location:../main");
+            }
+        } else {
+            if (is_file('./' . $pagina[0] . '.php')) {
+                include $pagina[0] . '.php';
+            } else {
+                echo "<script>alert ('Essa não e uma pagina valida!!') ; </script>";
+                header("Location:../main");
+            }
+        }
+    } else {
+        if (is_file('./' . $_GET['pg'] . '.php')) {
+            include $_GET['pg'] . '.php';
+        } else {
+            echo "<script>alert ('Essa não e uma pagina valida!!!') ; </script>";
+            header("Location:../");
+        }
+    }
+} else {
+    include "../index.php";
+}
